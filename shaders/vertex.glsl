6cd1f39721ec2581c50d6f3099d9d33a3bcfeb31
NavasJorge2307 <jorgenavas230706@gmail.com>
2026-06-14 00:41:29 -0600

    Add files via upload

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -8,15 +8,26 @@ out vec3 FragPos;
 out vec3 Normal;
 out vec2 TexCoords;
 
+out vec4 FragPosFlashLightSpace;
+out vec4 FragPosLampLightSpace;
+
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+uniform mat4 flashLightSpaceMatrix;
+uniform mat4 lampLightSpaceMatrix;
+
 void main()
 {
-    FragPos = vec3(model * vec4(aPos, 1.0));
+    vec4 worldPos = model * vec4(aPos, 1.0);
+
+    FragPos = worldPos.xyz;
     Normal = mat3(transpose(inverse(model))) * aNormal;
     TexCoords = aTexCoords;
 
-    gl_Position = projection * view * vec4(FragPos, 1.0);
+    FragPosFlashLightSpace = flashLightSpaceMatrix * worldPos;
+    FragPosLampLightSpace = lampLightSpaceMatrix * worldPos;
+
+    gl_Position = projection * view * worldPos;
 }
